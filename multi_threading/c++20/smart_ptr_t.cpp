32554d2ce80873991054f9c125a90b4cf4165db2
WenTao Sun <sunwt5@lenovo.com>
2024-11-22 15:02:17 +0800

    Update cpp project

--- multi_threading/c++20/smart_ptr_t.cpp
+++ multi_threading/c++20/smart_ptr_t.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <thread>
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::endl;
@@ -8,8 +9,29 @@ using std::thread;
 using std::unique_ptr;
 using std::shared_ptr;
 using std::weak_ptr;
+using std::string;
 
 class MyStruct;
+
+weak_ptr<int> gw;
+
+void observe() {
+	cout << "gw.use_count = " << gw.use_count() << "; \n";
+	if (shared_ptr<int> spt = gw.lock())
+		cout << " *spt = " << *spt << "\n";
+	else
+		cout << "gw is expired.\n";
+}
+
+void test_weakptr() {
+	{
+		auto sp = std::make_shared<int>(42);
+		gw = sp;
+
+		observe();
+	}
+	observe();
+}
 
 void foo() {
 	unique_ptr<int> up1 = std::make_unique<int>(2024);
@@ -38,9 +60,46 @@ private:
 
 int MyStruct::count = 0;
 
+// https://www.bilibili.com/video/BV1RD4y1W7hf
+struct School;
+
+struct Teacher {
+	string name;
+	int age;
+	shared_ptr<School> school;
+	~Teacher() {
+		cout << "Teacher destructed.\n";
+	}
+};
+
+struct School {
+	string name;
+	shared_ptr<Teacher> principle;
+	~School() {
+		cout << "School destructed.\n";
+	}
+};
+
+void test_sharedptr() {
+	auto principle = std::make_shared<Teacher>();
+	auto university = std::make_shared<School>();
+
+	principle->school = university;
+	university->principle = principle;
+
+	cout << "Principle use count = " << principle.use_count() << endl;
+	cout << "School use count = " << university.use_count() << endl;
+}
+
 int main() {
 	thread t1(foo);
 	t1.join();
+
+	thread t2(test_weakptr);
+	t2.join();
+
+	thread t3(test_sharedptr);
+	t3.join();
 
 	return 0;
 }
